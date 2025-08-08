eacaacb7c698f655c0d3a6400be0509d27b4382a
bassemeddouzi <bassemeddouzi@gmail.com>
2025-08-08 15:20:06 +0100

    aaa

---
+++ Backend/ExemplData.js
@@ -0,0 +1,338 @@
+const doctorProfile = {
+    "id": 6,
+    "userId": 16,
+    "firstName": "Chandler",
+    "lastName": "Walsh-Rogahn",
+    "photoId": 67,
+    "professionalBio": "Solutio sordeo adulescens. Tenuis theatrum surgo curis unde amplexus amplus arx sursum utpote. Placeat conqueror subnecto terreo succedo adulatio aranea decor videlicet audacia.",
+    "specialization": "Family Medicine",
+    "yearsExperience": 18,
+    "medicalLicense": "MD-Co-56896",
+    "officeAddress": "89730 Stoney Lane",
+    "city": "North Kareem",
+    "state": "Ohio",
+    "zipCode": "00897",
+    "phoneNumber": "285-250-1653",
+    "emergencyContact": "361.560.8316",
+    "availability": {
+        "friday": [
+            "09:00-13:00",
+            "14:00-16:00"
+        ],
+        "monday": [
+            "09:00-13:00",
+            "14:00-17:00"
+        ],
+        "tuesday": [
+            "09:00-13:00",
+            "14:00-17:00"
+        ],
+        "thursday": [
+            "09:00-13:00",
+            "14:00-17:00"
+        ],
+        "wednesday": [
+            "09:00-13:00",
+            "14:00-17:00"
+        ]
+    },
+    "avgReview": 2.666666666666667,
+    "user": {
+        "id": 16,
+        "email": "Chandler_Walsh-Rogahn54@healthyconnect.com",
+        "role": "DOCTOR",
+        "createdAt": "2025-08-08T13:55:43.895Z",
+        "updatedAt": "2025-08-08T13:55:43.895Z"
+    },
+    "photo": {
+        "id": 67,
+        "ownerId": 16,
+        "url": "https://storage.healthyconnect.com/avatars/doctor5.jpg",
+        "fileType": "PROFILE_PICTURE",
+        "createdAt": "2025-08-08T13:55:45.941Z"
+    },
+    "appointments": [
+        {
+            "id": 22,
+            "doctorId": 6,
+            "patientId": 15,
+            "date": "2025-10-18T12:52:49.473Z",
+            "status": "PENDING",
+            "type": "TEXT",
+            "reason": "Decor cupio terra vix artificiose tergo.",
+            "notes": "Voluptatibus doloribus vere verbum tardus cunabula. Absconditus cogo facere similique veritas vito vero sperno amaritudo. Via utpote sponte absconditus libero.",
+            "recordingId": null,
+            "createdAt": "2025-08-08T13:55:46.474Z",
+            "patient": {
+                "id": 15,
+                "userId": 25,
+                "firstName": "Dewayne",
+                "lastName": "Brown",
+                "photoId": 89,
+                "phoneNumber": "1-553-881-2645",
+                "dateOfBirth": "1985-01-05T16:21:47.336Z",
+                "address": "781 Low Road",
+                "city": "Skylarberg",
+                "state": "Indiana",
+                "zipCode": "57151",
+                "medicalHistory": "Tergo a textilis. Causa asporto tam solutio collum vulgivagus alveus territo utique acer. Cursim adiuvo clam.",
+                "user": {
+                    "email": "Dewayne.Brown55@yahoo.com"
+                }
+            },
+            "recording": null,
+            "messages": []
+        },
+        {
+            "id": 26,
+            "doctorId": 6,
+            "patientId": 15,
+            "date": "2026-02-03T04:07:15.185Z",
+            "status": "CONFIRMED",
+            "type": "VIDEO",
+            "reason": "Hic paens veniam amor tui ea usitas ad chirographum.",
+            "notes": "Patria depraedor vorago. Venustas capto quidem. Utique tero pectus deorsum tricesimus suppono pecco vox clementia auditor.",
+            "recordingId": null,
+            "createdAt": "2025-08-08T13:55:46.519Z",
+            "patient": {
+                "id": 15,
+                "userId": 25,
+                "firstName": "Dewayne",
+                "lastName": "Brown",
+                "photoId": 89,
+                "phoneNumber": "1-553-881-2645",
+                "dateOfBirth": "1985-01-05T16:21:47.336Z",
+                "address": "781 Low Road",
+                "city": "Skylarberg",
+                "state": "Indiana",
+                "zipCode": "57151",
+                "medicalHistory": "Tergo a textilis. Causa asporto tam solutio collum vulgivagus alveus territo utique acer. Cursim adiuvo clam.",
+                "user": {
+                    "email": "Dewayne.Brown55@yahoo.com"
+                }
+            },
+            "recording": null,
+            "messages": []
+        },
+        {
+            "id": 28,
+            "doctorId": 6,
+            "patientId": 19,
+            "date": "2026-05-29T07:38:27.963Z",
+            "status": "PENDING",
+            "type": "TEXT",
+            "reason": "Uxor suspendo carbo ratione summisse cohaero torqueo vere sollers consuasor.",
+            "notes": "Crastinus somnus candidus dolore creptio synagoga utilis. Vomica voro pecto tempora deduco. Mollitia adaugeo cupiditate conduco.",
+            "recordingId": null,
+            "createdAt": "2025-08-08T13:55:46.539Z",
+            "patient": {
+                "id": 19,
+                "userId": 29,
+                "firstName": "Citlalli",
+                "lastName": "Monahan",
+                "photoId": 93,
+                "phoneNumber": "663.459.3302 x29511",
+                "dateOfBirth": "1982-02-25T16:13:10.343Z",
+                "address": "3269 S 1st Street",
+                "city": "Dareboro",
+                "state": "New Mexico",
+                "zipCode": "40018",
+                "medicalHistory": "Argumentum adflicto apud thymum ambitus antiquus tergo. Alveus tertius harum. Peccatus adhuc adflicto confugo decens.",
+                "user": {
+                    "email": "Citlalli.Monahan12@hotmail.com"
+                }
+            },
+            "recording": null,
+            "messages": []
+        },
+        {
+            "id": 36,
+            "doctorId": 6,
+            "patientId": 15,
+            "date": "2025-10-15T07:57:41.692Z",
+            "status": "PENDING",
+            "type": "TEXT",
+            "reason": "Bene alii ea conculco infit.",
+            "notes": "Corroboro vehemens demulceo conqueror terra damno cuius desparatus corporis speculum. Eos molestiae vix subnecto absum aeger compono aggredior colligo. Curia desino spectaculum distinctio calcar turpis cognomen ubi.",
+            "recordingId": null,
+            "createdAt": "2025-08-08T13:55:46.605Z",
+            "patient": {
+                "id": 15,
+                "userId": 25,
+                "firstName": "Dewayne",
+                "lastName": "Brown",
+                "photoId": 89,
+                "phoneNumber": "1-553-881-2645",
+                "dateOfBirth": "1985-01-05T16:21:47.336Z",
+                "address": "781 Low Road",
+                "city": "Skylarberg",
+                "state": "Indiana",
+                "zipCode": "57151",
+                "medicalHistory": "Tergo a textilis. Causa asporto tam solutio collum vulgivagus alveus territo utique acer. Cursim adiuvo clam.",
+                "user": {
+                    "email": "Dewayne.Brown55@yahoo.com"
+                }
+            },
+            "recording": null,
+            "messages": []
+        },
+        {
+            "id": 39,
+            "doctorId": 6,
+            "patientId": 14,
+            "date": "2025-08-22T06:35:11.293Z",
+            "status": "COMPLETED",
+            "type": "TEXT",
+            "reason": "Pel crapula vulgo defessus vitiosus debilito.",
+            "notes": "Victus volo bibo virtus deorsum calco curriculum viduo. Sursum cibus depereo cras adulatio degusto tergiversatio trepide vicinus. Adaugeo desparatus arbor censura vos apparatus cedo vinco.",
+            "recordingId": null,
+            "createdAt": "2025-08-08T13:55:46.627Z",
+            "patient": {
+                "id": 14,
+                "userId": 24,
+                "firstName": "Cordell",
+                "lastName": "Hackett",
+                "photoId": 88,
+                "phoneNumber": "1-340-822-4357 x695",
+                "dateOfBirth": "1991-10-22T03:54:46.031Z",
+                "address": "419 Ledner Key",
+                "city": "Lake Clintonport",
+                "state": "North Dakota",
+                "zipCode": "05208",
+                "medicalHistory": "A campana cumque. Hic ocer desparatus copia. Villa cohibeo delinquo.",
+                "user": {
+                    "email": "Cordell.Hackett59@hotmail.com"
+                }
+            },
+            "recording": null,
+            "messages": []
+        }
+    ],
+    "reviews": [
+        {
+            "id": 18,
+            "doctorId": 6,
+            "patientId": 13,
+            "rating": 2,
+            "comment": "Via atque avarus deleo demitto. Benigne theca thalassinus a conturbo creber decimus ipsam caste. Depereo aspernatur tantillus delego esse valetudo aegrus coepi.",
+            "createdAt": "2025-08-08T13:55:46.656Z",
+            "patient": {
+                "id": 13,
+                "userId": 23,
+                "firstName": "Ian",
+                "lastName": "Rowe",
+                "photoId": 87,
+                "phoneNumber": "1-933-394-2613 x42134",
+                "dateOfBirth": "1970-07-15T09:46:39.722Z",
+                "address": "239 Saige Coves",
+                "city": "Sunrise",
+                "state": "California",
+                "zipCode": "67772",
+                "medicalHistory": "Coniuratio tempus beatus animadverto tui. Tepesco rem averto atque adficio strues sponte sumo damnatio. Sapiente spes recusandae antea convoco incidunt sophismata cupiditate curvo theca.",
+                "user": {
+                    "email": "Ian_Rowe1@yahoo.com"
+                }
+            }
+        },
+        {
+            "id": 19,
+            "doctorId": 6,
+            "patientId": 16,
+            "rating": 2,
+            "comment": "Umquam utilis tollo civitas celebrer dolorum textus deporto. Numquam dolor inflammatio tendo sunt explicabo. Denique solvo sortitus stips adulescens alo peccatus.",
+            "createdAt": "2025-08-08T13:55:46.665Z",
+            "patient": {
+                "id": 16,
+                "userId": 26,
+                "firstName": "Deondre",
+                "lastName": "Heller",
+                "photoId": 90,
+                "phoneNumber": "969-684-7573 x957",
+                "dateOfBirth": "1989-05-15T08:18:14.446Z",
+                "address": "62867 Pierce Canyon",
+                "city": "New Shanelle",
+                "state": "North Carolina",
+                "zipCode": "30693",
+                "medicalHistory": "Acidus cicuta cupressus. Appono quisquam vinculum distinctio acies versus distinctio. Cunabula adipiscor tenetur delego defero casso comparo ipsum capto.",
+                "user": {
+                    "email": "Deondre.Heller@gmail.com"
+                }
+            }
+        },
+        {
+            "id": 22,
+            "doctorId": 6,
+            "patientId": 16,
+            "rating": 4,
+            "comment": "Vindico triumphus volva vulnus tener volubilis tondeo. Auctus turpis vero cotidie aiunt tam pauper recusandae umerus. Ante verumtamen nemo.",
+            "createdAt": "2025-08-08T13:55:46.696Z",
+            "patient": {
+                "id": 16,
+                "userId": 26,
+                "firstName": "Deondre",
+                "lastName": "Heller",
+                "photoId": 90,
+                "phoneNumber": "969-684-7573 x957",
+                "dateOfBirth": "1989-05-15T08:18:14.446Z",
+                "address": "62867 Pierce Canyon",
+                "city": "New Shanelle",
+                "state": "North Carolina",
+                "zipCode": "30693",
+                "medicalHistory": "Acidus cicuta cupressus. Appono quisquam vinculum distinctio acies versus distinctio. Cunabula adipiscor tenetur delego defero casso comparo ipsum capto.",
+                "user": {
+                    "email": "Deondre.Heller@gmail.com"
+                }
+            }
+        }
+    ],
+    "certifications": [
+        {
+            "id": 16,
+            "doctorProfileId": 6,
+            "fileId": 68,
+            "file": {
+                "id": 68,
+                "ownerId": 16,
+                "url": "https://storage.healthyconnect.com/certifications/board-certification1.pdf",
+                "fileType": "CERTIFICATION",
+                "createdAt": "2025-08-08T13:55:45.973Z"
+            }
+        },
+        {
+            "id": 17,
+            "doctorProfileId": 6,
+            "fileId": 69,
+            "file": {
+                "id": 69,
+                "ownerId": 16,
+                "url": "https://storage.healthyconnect.com/certifications/specialization1.pdf",
+                "fileType": "CERTIFICATION",
+                "createdAt": "2025-08-08T13:55:45.993Z"
+            }
+        },
+        {
+            "id": 18,
+            "doctorProfileId": 6,
+            "fileId": 70,
+            "file": {
+                "id": 70,
+                "ownerId": 16,
+                "url": "https://storage.healthyconnect.com/certifications/training-certificate1.pdf",
+                "fileType": "CERTIFICATION",
+                "createdAt": "2025-08-08T13:55:46.011Z"
+            }
+        },
+        {
+            "id": 19,
+            "doctorProfileId": 6,
+            "fileId": 71,
+            "file": {
+                "id": 71,
+                "ownerId": 16,
+                "url": "https://storage.healthyconnect.com/certifications/specialization1.pdf",
+                "fileType": "CERTIFICATION",
+                "createdAt": "2025-08-08T13:55:46.026Z"
+            }
+        }
+    ]
+}
