12749d301881f39327dc71567561c1656dad7623
MohamedChaibi <hamma_chaibi@yahoo.fr>
2025-08-12 11:38:24 +0100

    wip

--- Backend/src/routes/userSettings.routes.js
+++ Backend/src/routes/userSettings.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import userSettingsController from '../controllers/userSettings.controller.js';
+import userSettingsController from '../controllers/userSettings.controller.js'; 
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
